02935b94e9b6913f5249e5f3671d9dfe7ad13bf7
marioa98 <mmartinez53@ucol.mx>
2019-09-02 21:06:36 -0500

    [ADD] Google Oauth

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
 let port = process.env.PORT || 4000;
 
-app.get('/', function(req, res){
-    res.send('<html><head></head><body> <h1>Hola Mundo</h1> </body></html>');
-});
+passport.use(new googleStrategy());
 
-app.listen(port);
+// app.get('/', function (req, res) {
+//     res.send('<html><head></head><body> <h1>Hola Mundo</h1> </body></html>');
+// });
+
+app.listen(port, () => console.log(`Escuchando en el puerto ${port}`));
