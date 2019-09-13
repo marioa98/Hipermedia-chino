f32a71ad8ebf2c77a8b2fcf443e65e8e6005ed0d
marioa98 <mmartinez53@ucol.mx>
2019-09-13 07:16:53 -0500

    [IDK] No se

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 require('./services/passport');
-const authRoutes = require('./routes/authRouthes');
 
 const app = express();
 
 let port = process.env.PORT || 4000;
 
-authRoutes(app);
+require('./routes/authRouthes')(app);
 
 app.listen(port, () => console.log(`Escuchando en el puerto ${port}`));
