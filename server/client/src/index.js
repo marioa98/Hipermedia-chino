6ab336abd21d1c7b6b23b867b02e16893788859f
marioa98 <mmartinez53@ucol.mx>
2019-10-25 08:28:53 -0500

    Ya funciona el cliente y servidor, con los reductores incluidos

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -4,10 +4,11 @@ import {Provider} from 'react-redux';
 import { createStore, applyMiddleware } from "redux";
 
 import App from './components/App'
+import reducers from './reducers'
 
-const store = createStore(() => [], {}, applyMiddleware);
+const store = createStore(() => reducers, {}, applyMiddleware());
 
 ReactDOM.render(
-    <Provider store={store}></Provider>,
+    <Provider store={store}><App/></Provider>,
     document.querySelector('#root')
 );
