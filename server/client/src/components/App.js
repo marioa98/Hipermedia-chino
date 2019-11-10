2a9eb189614c72020d1c8310f6d9bc42e2e564fa
marioa98 <mmartinez53@ucol.mx>
2019-11-10 12:31:53 -0600

    Cambios de la clase del 08 de noviembre de 2019

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
+import {connect} from 'react-redux';
+import * as actions from '../actions'
 
 import Header  from './Header'
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveNew = () => <h2>SurveNew</h2>
 const Landing = () => <h2>Landing</h2>
 
-const App = ()=>{
-    return(
-        <div className="container">
-            <BrowserRouter>
-                <div>
-                    <Header />
-                    <Route exact path="/" component={Landing}/>
-                    <Route exact path="/surveys" component={Dashboard}/>
-                    <Route path="/surveys/new" component={SurveNew}/>
-                </div> 
-            </BrowserRouter>
-        </div>
-    );
+class App extends Component{
+
+    componentDidMount() {
+        this.props.fetchUser();
+    }  
+    
+    render(){
+        return(
+            <div className="container">
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route exact path="/" component={Landing}/>
+                        <Route exact path="/surveys" component={Dashboard}/>
+                        <Route path="/surveys/new" component={SurveNew}/>
+                    </div> 
+                </BrowserRouter>
+            </div>
+        );
+    }
 };
 
-export default App;
+export default connect(null, actions)(App);
--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -5,9 +5,9 @@ class Header extends Component{
     render(){
         return(
             <nav>
-                <div class="nav-wrapper indigo">
-                <a href="#" class="left brand-logo">Emaily</a>
-                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <div className="nav-wrapper indigo">
+                <a href="tupu.html" className="left brand-logo">Emaily</a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li><a href="sass.html">Login with google</a></li>
                 </ul>
                 </div>
